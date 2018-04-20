52b1c48835cd187b4d0f70ff256449ccdae9e021
viviane <viviane.mmat@gmail.com>
2018-04-20 18:03:23 -0300

    primeira versao projeto 1

--- Projeto1/__init__.py
+++ Projeto1/__init__.py
@@ -1,21 +1,38 @@
 import glob
 from astropy.io import fits
 import numpy as np
+from numba import jit
 
-data_path = caminho          #'/home2/viviane15/Downloads/xo2b/'  
+'''
 
+git add --all
+git commit -m 'comentario'
+git push -u origin master
 
+'''
+#'/home2/viviane15/Downloads/xo2b/'  
+
+@jit
+def find_files(n):
+	paths = glob.glob(data_path + n)  
+	fit_files=[]                                     
+	for i in paths:                            
+		img = fits.getdata(i)                         
+		img = img.astype(np.float64)
+		fit_files.append(img)
+	
+	fit_files = np.array(fit_files)
+	return fit_files
+	
+# def save
+
+	
 
 def master_bias():
-	imagens_bias = glob.glob(data_path+'bias*.fits')  #caminhos
-	bias_fits=[]
-	for i in imagens_bias:
-		img = fits.getdata(i)
-		img = img.astype(np.float64)
-		bias_fits.append(img)
 	
-	bias_fits = np.array(bias_fits)
-	master_bias = np.median(bias_fits,axis=1) 
+	bias_files = find_files('bias*.fits')
+		
+	master_bias = np.median(bias_files,axis=0)         #tirando a mediana
 	
 	hdu = fits.PrimaryHDU()                #criando HDU
 	hdu.data = master_bias
@@ -24,18 +41,42 @@ def master_bias():
 	hdu.header['comment'] = 'Masterbias obtido a partir da mediana de tantas imagens'
 	hdu.writeto('master_bias.fits')
 	
+	return master_bias
 	
+
 	
-def masterFlat():
-	imagens_flats = glob.glob(data_path+'flat*.fits')
-	flats_fits=[]
+def	flat_b():
+	flat_files = find_files('flat*.fits')
+	mb = master_bias()
 	
-	for i in imagens_flats:
-		img = fits.getdata(i)
-		flats_fits.append(img)
+	flats_b = []
+	for i in flat_files:
+		f_b = i - mb 
+		flats_b.append(f_b)
 		
-	bias_fits = np.array(flats_fits)
-	master_flat = np.median(flats_fits, axis=1)
+	flats_b = np.array(flats_b)
+	return flats_b
+		
+	
+def normal_flat():
+	fb = flat_b()
+	
+	norms = []
+	for i in fb:   #???????????
+		med = np.median(i)
+		norm = i/med
+		norms.append(norm)
+		
+	norms = np.array(norms)
+	return norms
+	
+	
+
+
+def master_Flat():
+	n = normal_flat()  #???????
+		
+	master_flat = np.median(n, axis=0)
 	
 	hdu = fits.PrimaryHDU()                #criando HDU
 	hdu.data = master_flat
@@ -46,13 +87,34 @@ def masterFlat():
 	
 	
 
-def science():
-	science_files = glob.glob(data_path+'xo2b*.fits')
-	science_fits = []
+def science_b():
+	science_files = find_files('xo2b*.fits')
+	mb = master_bias() #??????
 	
+	science_b = []
 	for i in science_files:
-		img = fits.getdata(i)
-		science_files.append(img)
+		s_b = i - mb
+		science_b.append(s_b)
+		
+	science_b = np.array(science_b)
+	return science_b
+	
+	
+def science_bf():
+	
+	science_bf =[]	
+	for i in science_b:
+		s_bf = i/master_Flat()
+		science_bf.append(s_bf)
+		
+	science_bf = np.array(science_bf)
+	return science_bf
+	
+	
+		
+		
+		
+	
 		
 	
 	
