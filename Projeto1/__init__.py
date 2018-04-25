3503914d278583638919061fa56ba4bdbcee2713
viviane <viviane.mmat@gmail.com>
2018-04-25 18:08:16 -0300

    primeira versao projeto 1

--- Projeto1/__init__.py
+++ Projeto1/__init__.py
@@ -10,11 +10,15 @@ git commit -m 'comentario'
 git push -u origin master
 
 '''
-#'/home2/viviane15/Downloads/xo2b/'  
+
+	
+##'/home2/viviane15/Downloads/xo2b/' 
+
+data_path = '/home2/viviane15/Downloads/xo2b/'
 
 @jit
 def find_files(n):
-	paths = glob.glob(data_path + n)  
+	paths = np.sort(glob.glob(data_path + n))  
 	fit_files=[]                                     
 	for i in paths:                            
 		img = fits.getdata(i)                         
@@ -24,8 +28,16 @@ def find_files(n):
 	fit_files = np.array(fit_files)
 	return fit_files
 	
-# def save
 
+@jit	
+def save_fits(s, nome, comentario = False, pasta = ''):  #(s, comentario = False)
+	
+	hdu = fits.PrimaryHDU()
+	outfile = data_path + pasta + nome +'.fits'
+	hdu.data = s
+	hdu.header['comment'] = comentario
+	hdu.writeto(outfile)
+	
 	
 
 def master_bias():
@@ -33,17 +45,13 @@ def master_bias():
 	bias_files = find_files('bias*.fits')
 		
 	master_bias = np.median(bias_files,axis=0)         #tirando a mediana
+		
+	save_fits(master_bias, 'master_bias', 'Masterbias obtido a partir da mediana')
 	
-	hdu = fits.PrimaryHDU()                #criando HDU
-	hdu.data = master_bias
+	#return master_bias  #img
+	print('feito')
 	
-	hdu.header['master_bias'] = True
-	hdu.header['comment'] = 'Masterbias obtido a partir da mediana de tantas imagens'
-	hdu.writeto('master_bias.fits')
-	
-	return master_bias
-	
-
+master_bias()
 	
 def	flat_b():
 	flat_files = find_files('flat*.fits')
@@ -55,8 +63,10 @@ def	flat_b():
 		flats_b.append(f_b)
 		
 	flats_b = np.array(flats_b)
-	return flats_b
 		
+	return flats_b  #lista de imgs
+		
+	
 	
 def normal_flat():
 	fb = flat_b()
@@ -68,8 +78,8 @@ def normal_flat():
 		norms.append(norm)
 		
 	norms = np.array(norms)
-	return norms
 	
+	return norms  #lista de imgs
 	
 
 
@@ -78,14 +88,11 @@ def master_Flat():
 		
 	master_flat = np.median(n, axis=0)
 	
-	hdu = fits.PrimaryHDU()                #criando HDU
-	hdu.data = master_flat
+	save_fits(master_flat, 'master_flat', 'Masterflat obtido a partir da mediana')
 	
-	hdu.header['masterflat'] = True
-	hdu.header['comment'] = 'Masterflat obtido a partir da mediana de tantas imagens'
-	hdu.writeto('master_flat.fits')
+	return master_flat  #img
 	
-	
+
 
 def science_b():
 	science_files = find_files('xo2b*.fits')
@@ -100,14 +107,20 @@ def science_b():
 	return science_b
 	
 	
+	
 def science_bf():
 	
-	science_bf =[]	
-	for i in science_b:
+	science_bf =[]
+	cont = 0	
+	for i in science_b():
 		s_bf = i/master_Flat()
 		science_bf.append(s_bf)
 		
+		save_fits(i, 'xo2b_bf_'+ str(cont), 'imagem de ciencia bf', 'ciencia')  #????? n funciona
+		cont+=1	
+		
 	science_bf = np.array(science_bf)
+	
 	return science_bf
 	
 	
